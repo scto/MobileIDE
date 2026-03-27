d0e62b28b0ec4bffaf54c120a2f1d8f27d145ebd
scto <tschmid35@gmail.com>
2026-03-27 15:48:43 +0100

    feat: rework

--- app/src/main/java/com/mobileide/app/viewmodel/IDEViewModel.kt
+++ app/src/main/java/com/mobileide/app/viewmodel/IDEViewModel.kt
@@ -247,10 +247,6 @@ class IDEViewModel(application: Application) : AndroidViewModel(application) {
     init {
         Logger.info(LogTag.VIEW_MODEL, "IDEViewModel created")
         loadProjects()
-        initializeWorkspace()
-    }
-
-    private fun initializeWorkspace() {
         viewModelScope.launch {
             val ws = workspace.load()
             _onboardingComplete.value = ws.onboardingComplete
