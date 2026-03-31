c7420217b08dc8cb9aabcb2959e0ab9110401852
scto <tschmid35@gmail.com>
2026-03-31 04:46:08 +0000

    feat: termux

--- app/src/main/cpp/vcspace.cpp
+++
@@ -1,9 +0,0 @@
-#include "gemini.h"
-#include <jni.h>
-#include <string>
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_mobileide_app_core_Secrets_getGenerativeAiApiKey(JNIEnv* env, jclass clazz) {
-    std::string key = MobileIDE::getGeminiKey();
-    return env->NewStringUTF(key.c_str());
-}
--- termux/emulator/src/main/java/com/mobileide/termux/emulator/Keep.kt
+++
@@ -1,2 +0,0 @@
-// Placeholder file to ensure directory is recognized by Git and Gradle
-package com.mobileide.termux.emulator
