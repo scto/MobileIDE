f6f9ac81668d364eebe9402ef3ebb576df2f115f
Thomas Schmid <1441369+scto@users.noreply.github.com>
2026-06-05 14:29:27 +0200

    Refactor MyApplication and add temp directory handling
    Refactor MyApplication to App class and add temp directory management.

--- app/src/main/java/com/scto/mobile/ide/ui/terminal/MyApplication.kt
+++ app/src/main/java/com/scto/mobile/ide/ui/terminal/MyApplication.kt
@@ -19,19 +19,71 @@
 package com.scto.mobile.ide.ui.terminal
 
 import android.app.Application
+import android.os.Build
+import android.os.StrictMode
+
+import com.github.anrwatchdog.ANRWatchDog
+
 import com.rk.libcommons.application
 import com.rk.resources.Res
+import com.rk.update.UpdateManager
 
-class MyApplication : Application() {
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+import java.io.File
+import java.util.concurrent.Executors
+
+class App : Application() {
+    @OptIn(DelicateCoroutinesApi::class)
+    companion object {
+        fun getTempDir(): File {
+            val tmp = File(application!!.filesDir.parentFile, "tmp")
+            if (!tmp.exists()) {
+                tmp.mkdir()
+            }
+            return tmp
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate() {
         super.onCreate()
-
-        // 1. Initialize the global Context of ReTerminal
         application = this
-
-        // 2. Initialize the resource module
         Res.application = this
 
-        // 3. (Optional) If you copied CrashHandler, you can also initialize it here.
+        GlobalScope.launch(Dispatchers.IO) {
+            getTempDir().apply {
+                if (exists() && listFiles().isNullOrEmpty().not()){ deleteRecursively() }
+            }
+        }
+
+        //Thread.setDefaultUncaughtExceptionHandler(CrashHandler)
+        ANRWatchDog().start()
+
+        UpdateManager().onUpdate()
+
+        if (BuildConfig.DEBUG){
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder().apply {
+                    detectAll()
+                    penaltyLog()
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+                        penaltyListener(Executors.newSingleThreadExecutor()) { violation ->
+                            println(violation.message)
+                            violation.printStackTrace()
+                            violation.cause?.let { throw it }
+                            println("vm policy error")
+                        }
+                    }
+                }.build()
+            )
+        }
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
     }
 }
