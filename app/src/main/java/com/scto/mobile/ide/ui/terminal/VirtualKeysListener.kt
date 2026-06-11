dc29db2a858056cc05cf32bdb34aa8717958478b
scto <tschmid35@gmail.com>
2026-06-11 17:31:36 +0200

    fix: gradle

--- app/src/main/java/com/scto/mobile/ide/ui/terminal/VirtualKeysListener.kt
+++ app/src/main/java/com/scto/mobile/ide/ui/terminal/VirtualKeysListener.kt
@@ -20,13 +20,21 @@ package com.scto.mobile.ide.ui.terminal
 
 import android.view.View
 import android.widget.Button
+
 import com.rk.terminal.ui.screens.terminal.virtualkeys.VirtualKeyButton
 import com.rk.terminal.ui.screens.terminal.virtualkeys.VirtualKeysView
+
 import com.termux.terminal.TerminalSession
 
-// 🔥 Directly port rk terminal's implementation
+// 🔥 Directly porting the implementation from rk terminal
 class VirtualKeysListener(val session: TerminalSession) : VirtualKeysView.IVirtualKeysView {
-    override fun onVirtualKeyButtonClick(view: View?, buttonInfo: VirtualKeyButton?, button: Button?) {
+
+    override fun onVirtualKeyButtonClick(
+        view: View?,
+        buttonInfo: VirtualKeyButton?,
+        button: Button?,
+    ) {
+
         val key = buttonInfo?.key ?: return
         val writeable: String =
             when (key) {
