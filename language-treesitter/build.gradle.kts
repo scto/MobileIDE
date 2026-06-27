12af5804195fb30159ade70475551c99cda244e5
scto <tschmid35@gmail.com>
2026-06-27 16:39:21 +0200

    refactor: changed treesitter libs

--- language-treesitter/build.gradle.kts
+++ language-treesitter/build.gradle.kts
@@ -89,7 +89,8 @@ android {
 
 dependencies {
     compileOnly(project(":editor"))
-    api(libs.tree.sitter)
+    api(libs.androidide.ts)
+    //api(libs.tree.sitter)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.junit)
     androidTestImplementation(libs.androidx.test.espresso)
