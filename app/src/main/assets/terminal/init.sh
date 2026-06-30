049f41295c89dc35110ced92d3d4603018836330
scto <tschmid35@gmail.com>
2026-06-30 16:42:00 +0200

    Show interactive notice to run idesetup instead of calling it directly during initial boot setup script to avoid TTY read error

--- app/src/main/assets/terminal/init.sh
+++ app/src/main/assets/terminal/init.sh
@@ -128,3 +128,7 @@ echo -e "  \e[1;33mapt upgrade\e[0m  : Upgrade packages"
 echo -e "  \e[1;33mapt install\e[0m  : Install <package>"
 echo -e "\e[1;30m----------------------------------------\e[0m"
 echo ""
+
+if [ ! -f "/etc/mobileide-environment.properties" ]; then
+    echo -e "\e[1;33mHint: Please run the '\e[1;32midesetup\e[1;33m' command to configure your Java & Android SDK build tools.\e[0m"
+fi
