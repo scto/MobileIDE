848277fe44d4f0707a8c0ae1f7bc5684e3079d75
scto <tschmid35@gmail.com>
2026-07-16 08:40:08 +0200

    Mein neuer Commit-Text ohne Backup-Datei

--- plugins/java-lsp/compileDebug.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if exist output rd /s /q output
-
-call gradlew.bat assembleDebug || exit /b 1
-call gradlew.bat :app:createFinalZip || exit /b 1
--- plugins/java-lsp/compileRelease.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if exist output rd /s /q output
-
-call gradlew.bat assembleRelease || exit /b 1
-call gradlew.bat :app:createFinalZip || exit /b 1
