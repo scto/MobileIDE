263785e8553c4027681f756a233bf6e6bc0603ca
scto <tschmid35@gmail.com>
2026-07-30 05:07:54 +0200

    fix(phase5): correct plugin-store jvmTarget mismatch

--- features/plugin-store/build.gradle.kts
+++ features/plugin-store/build.gradle.kts
@@ -16,8 +16,11 @@ android {
         compose = true
     }
 
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    kotlin { compilerOptions { jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17) } }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
