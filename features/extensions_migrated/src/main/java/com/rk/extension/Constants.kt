8b74813508cae02e282e6fd4574a9e87bb25483b
scto <tschmid35@gmail.com>
2026-07-31 20:30:07 +0200

    chore: remove unused features/extensions_migrated leftover

--- features/extensions_migrated/src/main/java/com/rk/extension/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.scto.mobile.ide.features.extensions
-
-const val EXTENSION_API_BASE="https://mobileide.app/api/extensions"
-const val THEMES_API_BASE="https://mobileide.app/api/themes"
-const val ICONPACKS_API_BASE="https://mobileide.app/api/icon-packs"
--- features/extensions_migrated/src/main/java/com/rk/extension/ExtensionBridge.kt
+++
@@ -1,5 +0,0 @@
-package com.scto.mobile.ide.features.extensions
-
-import com.scto.mobile.ide.features.extensions.manager.ExtensionManager
-
-lateinit var extensionManager: ExtensionManager
