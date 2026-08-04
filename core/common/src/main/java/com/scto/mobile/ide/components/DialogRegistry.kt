d9c709a95e48659bf7e8fd094f3c36953bec58f6
scto <tschmid35@gmail.com>
2026-08-04 16:08:27 +0200

    fix(compose): add register/unregister/clear helpers to DialogRegistry to prevent Compose snapshot thread access exceptions

--- core/common/src/main/java/com/scto/mobile/ide/components/DialogRegistry.kt
+++ core/common/src/main/java/com/scto/mobile/ide/components/DialogRegistry.kt
@@ -5,4 +5,18 @@ import androidx.compose.runtime.mutableStateListOf
 
 object DialogRegistry {
     val dialogs = mutableStateListOf<@Composable () -> Unit>()
+
+    fun register(dialog: @Composable () -> Unit) {
+        if (!dialogs.contains(dialog)) {
+            dialogs.add(dialog)
+        }
+    }
+
+    fun unregister(dialog: @Composable () -> Unit) {
+        dialogs.remove(dialog)
+    }
+
+    fun clear() {
+        dialogs.clear()
+    }
 }
